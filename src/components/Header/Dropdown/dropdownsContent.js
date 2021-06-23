71a8b641fefc8f9970a4d6d6dfe8a79f08d2f600
Vadim Budko <viaduksun@gmail.com>
2021-06-23 15:04:22 +0300

    lint

--- src/components/Header/Dropdown/dropdownsContent.js
+++ src/components/Header/Dropdown/dropdownsContent.js
@@ -1,179 +1,179 @@
 const dropdownsContent = [
-  {
-    categoryId: 1,
-    dropContent: [
-      {
-        title: 'Фотоаппараты',
-        path: '/products',
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Видеокамеры',
-        path: '/products',
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Вспышки',
-        path: '/products',
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Экшн камеры',
-        path: '/products',
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Карты памяти',
-        path: '/products',
-        id: '05_MenuItems'
-      },
-      {
-        title: 'Кардридеры',
-        path: '/products',
-        id: '06_MenuItems'
-      },
-      {
-        title: 'USB флэш накопители',
-        path: '/products',
-        id: '07_MenuItems'
-      },
-      {
-        title: 'Внешние жесткие диски',
-        path: '/products',
-        id: '08_MenuItems'
-      },
-      {
-        title: 'Фотопринтеры',
-        path: '/products',
-        id: '09_MenuItems'
-      },
-      {
-        title: 'Фотобумага для фотопринтера',
-        path: '/products',
-        id: '10_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 2,
-    dropContent: [
-      {
-        title: 'Оптика для фото',
-        path: '/products',
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-        path: '/products',
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-        path: '/products',
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-        path: '/products',
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Оптика для фото',
-        path: '/products',
-        id: '05_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 3,
-    dropContent: [
-      {
-        title: 'Аксессуары',
-        path: '/products',
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-        path: '/products',
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-        path: '/products',
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-        path: '/products',
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Аксессуары',
-        path: '/products',
-        id: '05_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 4,
-    dropContent: [
-      {
-        title: 'Чехлы',
-        path: '/products',
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-        path: '/products',
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-        path: '/products',
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-        path: '/products',
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Чехлы',
-        path: '/products',
-        id: '05_MenuItems'
-      }
-    ]
-  },
-  {
-    categoryId: 5,
-    dropContent: [
-      {
-        title: 'Оборудование',
-        path: '/products',
-        id: '01_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-        path: '/products',
-        id: '02_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-        path: '/products',
-        id: '03_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-        path: '/products',
-        id: '04_MenuItems'
-      },
-      {
-        title: 'Оборудование',
-        path: '/products',
-        id: '05_MenuItems'
-      }
-    ]
-  },
+        {
+                categoryId: 1,
+                dropContent: [
+                        {
+                                title: 'Фотоаппараты',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Видеокамеры',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Вспышки',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Экшн камеры',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Карты памяти',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        },
+                        {
+                                title: 'Кардридеры',
+                                path: '/products',
+                                id: '06_MenuItems'
+                        },
+                        {
+                                title: 'USB флэш накопители',
+                                path: '/products',
+                                id: '07_MenuItems'
+                        },
+                        {
+                                title: 'Внешние жесткие диски',
+                                path: '/products',
+                                id: '08_MenuItems'
+                        },
+                        {
+                                title: 'Фотопринтеры',
+                                path: '/products',
+                                id: '09_MenuItems'
+                        },
+                        {
+                                title: 'Фотобумага для фотопринтера',
+                                path: '/products',
+                                id: '10_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 2,
+                dropContent: [
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Оптика для фото',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 3,
+                dropContent: [
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Аксессуары',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 4,
+                dropContent: [
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Чехлы',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
+        {
+                categoryId: 5,
+                dropContent: [
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '01_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '02_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '03_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '04_MenuItems'
+                        },
+                        {
+                                title: 'Оборудование',
+                                path: '/products',
+                                id: '05_MenuItems'
+                        }
+                ]
+        },
 ];
 
 export default dropdownsContent;
