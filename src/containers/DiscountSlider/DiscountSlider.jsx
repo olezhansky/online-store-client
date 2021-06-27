82c61cb98d6aea1d66bb8f5d8619231ff9ee8fff
Oleh Ivanov <fenix12895@gmail.com>
2021-06-27 12:24:34 +0300

    edits discount carousel component

--- src/containers/DiscountSlider/DiscountSlider.jsx
+++ src/containers/DiscountSlider/DiscountSlider.jsx
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import styles from './DiscountSlider.module.scss';
-
-// const DiscountSlider = () => (
-//   <div className="container">
-//     <div className={styles.DiscountSlider}>
-//       <h1>Discount Slider</h1>
-//     </div>
-//   </div>
-// );
-
-// export default DiscountSlider;
 import React from 'react';
 import SwiperCarousel from './SwiperCarousel/SwiperCarousel';
 import styles from './DiscountSlider.module.scss';
@@ -21,26 +9,40 @@ import img5 from '../../assets/img/discountSliderComponent/img5.png';
 
 const DiscountSlider = () => (
   <div className={styles.Sale}>
-    <div className={styles.slider}>
-      <SwiperCarousel />
-    </div>
-    <div className={styles.img1}>
-      <img src={img1} alt={img1} />
-    </div>
-    <div className={styles.img2}>
-      <img src={img2} alt={img1} />
-    </div>
-    <div className={styles.img3}>
-      <img src={img3} alt={img1} />
-    </div>
-    <div className={styles.img4}>
-      <img src={img4} alt={img1} />
-    </div>
-    <div className={styles.img5}>
-      <img src={img5} alt={img1} />
+    <div className={styles.SaleContainer}>
+      <h2 className={styles.SaleTitle}>АКЦИИ И СПЕЦПРЕДЛОЖЕНИЯ</h2>
+      <div className={styles.SaleSlider}>
+        <div className={styles.slider}>
+          <SwiperCarousel />
+        </div>
+        <div className={styles.img1}>
+          <div className={styles.img1Content}>
+            <p className={styles.img1ContentDiscount}>Скидка 10%</p>
+            <p className={styles.img1ContentPrice}>
+              1120
+              <span> грн</span>
+            </p>
+            <p className={styles.img1ContentTitle}>Benro DJ-80</p>
+          </div>
+          <div className={styles.img1Block}>
+            <img src={img1} alt={img1} />
+          </div>
+        </div>
+        <div className={styles.img2}>
+          <img src={img2} alt={img2} />
+        </div>
+        <div className={styles.img3}>
+          <img src={img3} alt={img3} />
+        </div>
+        <div className={styles.img4}>
+          <img src={img4} alt={img4} />
+        </div>
+        <div className={styles.img5}>
+          <img src={img5} alt={img5} />
+        </div>
+      </div>
     </div>
   </div>
-        
-    );
+);
 
 export default DiscountSlider;
