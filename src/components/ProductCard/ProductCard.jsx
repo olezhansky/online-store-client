6b4a110137693a3573ce95715d28abad7817338e
Anna Sakhatska <sakhatska.anna@gmail.com>
2021-06-29 21:48:39 +0300

    add product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BiCart } from 'react-icons/bi';
 import styles from './ProductCard.module.scss';
+import products from './products';
 
 const ProductCard = () => (
   <Link to="/single-product">
+    {/* {products.map(() => ( */}
     <div className={styles.ProductCard}>
-      <h2>Product card</h2>
+      <img
+        className={styles.ProductImg}
+        src="https://files.foxtrot.com.ua/PhotoNew/img_0_117_3047_0_Small.webp"
+        alt=""
+      />
+      <h2 className={styles.ProductName}>
+        NIKON Z 7 + 24-70 f4 + FTZ Adapter Kit
+        {products.name}
+      </h2>
+      <div className={styles.ProductBlock}>
+        <div className={styles.ProductData}>
+          <span className={styles.ProductPrice}>
+            103 615 ГРН
+            {products.price}
+          </span>
+          <span className={styles.ProductStatus}>
+            в наличии
+            {products.status}
+          </span>
+        </div>
+        <div>
+          <span>
+            <BiCart className={styles.ProductIcon} />
+          </span>
+        </div>
+      </div>
     </div>
+    {/* ))} */}
   </Link>
 );
 
