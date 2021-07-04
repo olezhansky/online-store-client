ae0e36a9b0efa97b59fcc1b296c37eb19640fb32
Valeriy Trots <affairs88@gmail.com>
2021-07-04 20:55:47 +0300

    change

--- src/components/Footer/FooterInformation.jsx
+++ src/components/Footer/FooterInformation.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Footer.module.scss';
 
-const FooterInformation = ({title, links}) => (
+const FooterInformation = ({title, links, id}) => (
   <div className={styles.FooterInformation}>
-    <h6>{title}</h6>
-    {links.map((link) => (
-      <p key={link.text} to={link.to}>
-        {link.text}
-      </p>
+    <input type="checkbox" id={id} className={styles.FooterInformationInput} />
+    <label htmlFor={id}>{title}</label>
+    <div className={styles.FooterInformationLinkWrapper}>
+      {links.map((link) => (
+        <p key={link.text} to={link.to} className={styles.FooterInformationText}>
+          {link.text}
+        </p>
       // <FooterLinkItem key={link.text} to={link.to} onClick={() => window.scrollTo(0, 0)}>
       //   {link.text}
       // </FooterLinkItem>
     ))}
+    </div>
   </div>
 );
 FooterInformation.propTypes = {
   title: PropTypes.string.isRequired,
-  // id: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
   links: PropTypes.arrayOf(PropTypes.shape({
     to: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
