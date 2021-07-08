78ed6aca0cb6733e5cc0cd812cf77947fb09cb10
Oleg Ivanov <u014@Altera2.local>
2021-07-08 13:58:24 +0300

    fix loader-css

--- src/components/ProductsField/ProductsField.jsx
+++ src/components/ProductsField/ProductsField.jsx
@@ -1,41 +1,23 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { useSelector } from 'react-redux';
 import styles from './ProductsField.module.scss';
 import ProductsSorting from '../ProductsSorting/ProductsSorting';
 import ProductCard from '../ProductCard/ProductCard';
 import Pagination from '../Pagination/Pagination';
-import getProducts from '../../api/getProducts';
-import Loader from '../UI/Loader/Loader';
 
-const ProductsField = () => {
-  // const [products, setProducts] = useState([]);
-  // useEffect(() => {
-  //   getProducts().then((productsDB) => {
-  //     setProducts(productsDB.data);
-  //   });
-  // }, []);
-
-  const products = useSelector((state) => state.products);
-  console.log(products);
-
-  return (
-    <div className={styles.ProductsField}>
-      
-      <ProductsSorting />
-      <h1>ProductsField</h1>
-      {products.length === 0 ? <Loader /> : (
-        <div className={styles.ProductsFieldGrid}>
-          {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
-        ))}
-        </div>
-)}
-      
-      <Pagination />
+const ProductsField = ({products}) => (
+  <div className={styles.ProductsField}>
+    <ProductsSorting />
+    <h1>ProductsField</h1>
+    <div className={styles.ProductsFieldGrid}>
+      {products.map((product) => (
+        <ProductCard product={product} key={product._id} />
+      ))}
     </div>
+    <Pagination />
+  </div>
   );
-};
 
 export default ProductsField;
