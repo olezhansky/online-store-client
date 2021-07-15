cc94cea6fd0c3d3f554f69a99251a8d2f42f861b
olezhansky <fenix12895@gmail.com>
2021-07-15 23:40:58 +0300

    fix-single-product

--- src/components/SingleProductBlock/SingleProductContent/SingleProductContent.jsx
+++ src/components/SingleProductBlock/SingleProductContent/SingleProductContent.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
 import Button from '../../UI/Button/Button';
 import styles from './SingleProductContent.module.scss';
 
@@ -7,17 +8,30 @@ const SingleProductContent = ({singleProduct}) => {
     console.log('test');
     return (
       <div className={styles.Wrapper}>
-        <p>{singleProduct.quantity !== 0 && <span>в наличии</span>}</p>
-        <p>
+        <p className={styles.InStock}>{singleProduct.quantity !== 0 && <span>в наличии</span>}</p>
+        <p className={styles.Price}>
           {singleProduct.currentPrice }
           {' '}
           грн
         </p>
-        <div className={styles.BuyButton}>
+        <div className={styles.ButtonBuy}>
           <Button addClass="cart_green">
-            Купить
+            <span>Купить</span>
+            <FiShoppingCart />
           </Button>
         </div>
+        <p className={styles.Delivery}>
+          Доставка
+        </p>
+        <ul className={styles.DeliveryList}>
+          <li>
+            • Доставка по всей Украине
+          </li>
+          <li>• Оплата товара при получении</li>
+          <li>
+            • Возможен самовывоз
+          </li>
+        </ul>
        
       </div>
     );
