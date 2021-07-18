0bbf560013a8cde248238b9b01b4bc9b666c98e0
Valeriy Trots <affairs88@gmail.com>
2021-07-18 17:58:23 +0300

    news static

---
+++ src/pages/Magazins.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Magazins = () => (
+  <div className="container">
+    <h2>МАГАЗИНЫ</h2>
+    <div>
+      <p>
+        a
+      </p>
+    </div>
+  </div>
+);
+
+export default Magazins;
