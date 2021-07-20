b396e9defdecab583e2f3e12add86b8a6afc32da
Oleg Ivanov <u014@Altera2.local>
2021-07-20 17:14:41 +0300

    breadcumbs

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,9 +4,10 @@ import FavoriteContainer from '../containers/FavoriteContainer/FavoriteContainer
 
 const Favorites = () => {
     console.log('favorites');
+    const array = [['/', 'Главная'], ['favorites', 'Избранное']];
     return (
       <>
-        <Breadcrumbs />
+        <Breadcrumbs data={array} />
         <FavoriteContainer />
       </>
     );
