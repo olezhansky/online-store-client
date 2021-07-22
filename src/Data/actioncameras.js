aee46887f076ddd02cd283c65d87e3e2f1ffcda8
Anna Sakhatska <sakhatska.anna@gmail.com>
2021-07-22 14:41:35 +0300

    products data

---
+++ src/Data/actioncameras.js
@@ -0,0 +1,59 @@
+export const actioncamera01 = {
+  brand: 'GOPRO',
+  name: 'GOPRO HERO9 Black',
+  artical: 3657568,
+  currentPrice: 12975,
+  previousPrice: 12975,
+  quantity: 21,
+  hitSale: 'да',
+  categories: 'actioncameras',
+  waranty: 12,
+  description:
+    'Знімайте приголомшливі відео з роздільною здатністю 5K і високою деталізацією навіть при наближенні і створюйте стоп-кадри 14,7 Мп. Також підтримується запис відео в 4K, 2.7K, 1440p і 1080p.Створюйте чіткі фотографії професійної якості з роздільною здатністю 20 Мп і використовуйте функцію «Суперфотка», щоб зробити кращий знімок. Новий модуль обmєктива Max*(в комплект не входить) забезпечує неперевершену стабілізацію Max HyperSmooth, а також Max SuperView – найширший кут огляду в історії GoPro.   ',
+  imageUrls: [
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626593492/OnlineStore/ActionCameras/GOPROHERO9Black/01_pakqdm.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626593492/OnlineStore/ActionCameras/GOPROHERO9Black/02_kg9hhm.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626593492/OnlineStore/ActionCameras/GOPROHERO9Black/03_opcfyt.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626593492/OnlineStore/ActionCameras/GOPROHERO9Black/04_oywd42.webp',
+  ],
+};
+
+export const actioncamera02 = {
+  brand: 'SONY',
+  name: 'SONY FDRX3000R.E35',
+  artical: 3987568,
+  currentPrice: 15799,
+  previousPrice: 15799,
+  quantity: 13,
+  hitSale: 'нет',
+  categories: 'actioncameras',
+  waranty: 12,
+  description:
+    'З вбудованою підтримкою GPS ви завжди будете бачити, де ви побували. Відстеження місця розташування і швидкості руху можна відобразити на зображенні при редагуванні відео з допомогою Action Cam Movie Creator.Знімайте в темряві і в напівтемряві завдяки передовим можливостям матриці. Створюйте бездоганно чіткі і динамічні відеоролики в Full HD зі швидкістю потоку 50 Мбіт/с.Зафіксуйте масштабні краєвиди з меншим рівнем спотворень.   ',
+  imageUrls: [
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953613/OnlineStore/ActionCameras/SONYFDRX3000R.E35/01_cepnpi.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953613/OnlineStore/ActionCameras/SONYFDRX3000R.E35/02_ov1ckt.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953613/OnlineStore/ActionCameras/SONYFDRX3000R.E35/03_bey93m.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953613/OnlineStore/ActionCameras/SONYFDRX3000R.E35/04_qqo2be.webp',
+  ],
+};
+
+export const actioncamera03 = {
+  brand: 'AIRON',
+  name: 'AIRON ProCam 7',
+  artical: 9637568,
+  currentPrice: 2555,
+  previousPrice: 2555,
+  quantity: 35,
+  hitSale: 'да',
+  categories: 'actioncameras',
+  waranty: 12,
+  description:
+    'Компактное устройство весом всего 91 грамм легко помещается в нагрудную сумку, чтобы всегда быть под рукой для особо крутых случаев. Time Lapse, Slow Motion, интервальная, зацикленная съемка, фото, видео с нужным разрешением и качеством. Ты можешь выбирать количество кадров в секунду для своих потребностей. Улучшенный LCD HD цветной сенсорный дисплей позволит тебе еще быстрее находить нужный элемент в меню, переключать режимы, экспериментировать с освещением и углом обзора. 150° широкоугольный объектив с 6 гироскопическими линзами поможет находить именно тот ракурс, от которого будут без ума все подписчики твоего профиля на ютубе или в инстаграме. При записи видео ты также можешь использовать смену угла обзора среди предоставленных широкого, среднего и узкого для еще большей динамики и акцентирования внимания на самых важных элементах твоей видео-истории.',
+  imageUrls: [
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953788/OnlineStore/ActionCameras/AIRONProCam7/01_jbrfzd.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953788/OnlineStore/ActionCameras/AIRONProCam7/02_itphhm.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953788/OnlineStore/ActionCameras/AIRONProCam7/03_zggvrw.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626953788/OnlineStore/ActionCameras/AIRONProCam7/04_mv4uze.webp',
+  ],
+};
---
+++ src/Data/videocameras.js
@@ -0,0 +1,59 @@
+export const videocamera01 = {
+  brand: 'PANASONIC',
+  name: 'PANASONIC HC-X1EE ',
+  artical: 597562,
+  currentPrice: 80999,
+  previousPrice: 80999,
+  quantity: 9,
+  hitSale: 'нет',
+  categories: 'videocameras',
+  waranty: 36,
+  description:
+    'Ця професійна відеокамера оснащена новим обьєктивом LEICA DICOMAR з дозволом 4K, фокусною відстанню 24 мм і оптичним 20-кратним зумом, а також великим високочутливим MOS-сенсором 1.0. вона надає рівень можливостей достатній для того, щоб перевести ваше відео виробництво на професійну картинку з роздільною здатністю 4K. Зйомка відео з роздільною здатністю 4К 60p/50p Чудова якість зображення і висока чутливість завдяки великому сенсору 1.0 з 4K. Великий сенсор 1.0 "(дюйм) (прибл. 9,46 мегапікселів при 4K24p і 8,79 мегапікселів в UHD, FHD) збалансований по глибині різкості, якості картинки і чутливості. Це дозволяє вести багатоформатну запис 4K24p, UHD 60p/50p і FHD 60p/50p. Новий обьєктив LEICA DICOMAR з роздільною здатністю 4К. Самий широкий кут 24мм*з представлених на ринку і оптичний 20-кратний зум. На додаток до найширшого на ринку кутку 24мм моделі з сенсором 1.0 "(дюйм) оснащені першим в світі ** 20- кратним оптичним зумом. Завдяки використанню 4-приводний оптичної системи, яка окремо управляє кожною з чотирьох груп лінз, нам вдалося добитися 20-кратного оптичного зума зберігши при цьому компактний корпус.',
+  imageUrls: [
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626587825/OnlineStore/Videocameras/PANASONICHC-X1EE/01_feg2cm.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626587825/OnlineStore/Videocameras/PANASONICHC-X1EE/02_xz6lm5.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626587825/OnlineStore/Videocameras/PANASONICHC-X1EE/03_fwpmlg.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626587825/OnlineStore/Videocameras/PANASONICHC-X1EE/04_oylwv2.webp',
+  ],
+};
+
+export const videocamera02 = {
+  brand: 'CANON',
+  name: 'CANON LEGRIA HF G50',
+  artical: 789568,
+  currentPrice: 41199,
+  previousPrice: 41199,
+  quantity: 8,
+  hitSale: 'да',
+  categories: 'videocameras',
+  waranty: 24,
+  description:
+    'Живіть моментом і створюйте історії в дозволі 4K з неймовірним рівнем деталізації завдяки простий у використанні відеокамері Canon, яка має оптичними компонентами преміум-класу і приголомшливим датчиком зображення. Новий датчик зображення для створення матеріалів в 4K забезпечує неймовірний рівень деталізації для сучасних 4K-телевізорів, в 8-пелюсткова діафрагма дозволяє створювати кінематографічний ефект розмиття світлих ділянок кадру. Знімайте плавні відео без тремтіння кадру завдяки потужному 20-кратному оптичному зуму, діафрагми f/1.8-2.8 і поліпшеною стабілізації зображення з функцією корекції повороту і нахилу. Переглядайте зображення у високій якості на сенсорному екрані 3,0 дюйма (460 000 точок) і використовуйте електронний видошукач з високою роздільною здатністю для зйомки в умовах яскравого освітлення і з нестандартних ракурсів.',
+  imageUrls: [
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694321/OnlineStore/Videocameras/CANONLEGRIAHFG50/01_fvadzf.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694321/OnlineStore/Videocameras/CANONLEGRIAHFG50/02_ldqulu.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694321/OnlineStore/Videocameras/CANONLEGRIAHFG50/03_tsmqgz.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694321/OnlineStore/Videocameras/CANONLEGRIAHFG50/04_qkc2al.webp',
+  ],
+};
+
+export const videocamera03 = {
+  brand: 'CANON',
+  name: 'CANON Legria HF G60',
+  artical: 987568,
+  currentPrice: 63699,
+  previousPrice: 63699,
+  quantity: 3,
+  hitSale: 'нет',
+  categories: 'videocameras',
+  waranty: 24,
+  description:
+    'Великий датчик зображення Canon типу 1.0 для створення матеріалів в 4K забезпечує високий рівень деталізації, відмінну чіткість зображення і перенесення кольорів, а 9-пелюсткова діафрагма дозволяє домогтися кінематографічно ефекту для світлих ділянок кадру. Знімайте яскраві і плавні відео без тремтіння кадру завдяки потужному 15-кратному оптичному зуму, діафрагми f/2.8-4.5, а також поліпшеною стабілізації зображення і технології Dual Pixel CMOS AF. Переглядайте зображення в високому як на сенсорному екрані 3,0 дюйма (460 000 точок) з швидким відгуком та використовуйте електронний видошукач з високою роздільною здатністю для зйомки з нестандартних ракурсів. 5 кнопок дозволяють налаштовувати елементи керування і регулювати швидкість фокусування для більш кінематографічною ефекту, а також керувати експозицією за допомогою вбудованих нейтральних фільтрів.   ',
+  imageUrls: [
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694658/OnlineStore/Videocameras/CANONLegriaHFG60/01_tpgzya.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694658/OnlineStore/Videocameras/CANONLegriaHFG60/02_k6ukbe.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694658/OnlineStore/Videocameras/CANONLegriaHFG60/03_fkdsjb.webp',
+    'https://res.cloudinary.com/finalprojectfe242021/image/upload/v1626694658/OnlineStore/Videocameras/CANONLegriaHFG60/04_j0hsl1.webp',
+  ],
+};
