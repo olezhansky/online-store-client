b19648ad427ccdc81948b61f4d8604f54d17998a
olezhansky <fenix12895@gmail.com>
2021-08-01 22:58:24 +0300

    commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,23 @@
 /* eslint-disable no-shadow */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Breadcrumbs from '../containers/Breadcrumbs/Breadcrumbs';
 import ProductsContainer from '../containers/ProductsContainer/ProductsContainer';
 import Stories from '../containers/Stories/Stories';
 
 const Products = () => {
-  const array = [['/', 'Главная'], ['products', 'Цифровая техника']];
+  const currentCategory = useSelector((state) => state.productsPage.currentCategory);
+  let activeBreadcrumbs = '';
+  if (currentCategory === 'photocameras') {
+    activeBreadcrumbs = 'Фотоаппараты';
+  } else if (currentCategory === 'videocameras') {
+    activeBreadcrumbs = 'Видеокамеры';
+  } else if (currentCategory === 'actioncameras') {
+    activeBreadcrumbs = 'Экшнкамеры';
+  } else if (currentCategory === 'lenses') {
+    activeBreadcrumbs = 'Объективы';
+  }
+  const array = [['/', 'Главная'], ['products', activeBreadcrumbs]];
   return (
     <>
       <Breadcrumbs data={array} />
