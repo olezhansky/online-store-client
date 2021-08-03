24f3c331d249836e455a1e580f3ddaddf549dcac
Vadim Budko <viaduksun@gmail.com>
2021-08-03 21:38:32 +0300

    profile

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -79,7 +79,11 @@ const ProductCard = ({ product }) => {
 
   return (
     <div className={styles.ProductCard}>
-      <Link to="/single-product" onClick={dispatchSingleProductHandler}>
+      <Link
+        to="/single-product"
+        onClick={dispatchSingleProductHandler}
+        className={styles.ProductCardImgLink}
+      >
         <div className={styles.ProductCardImg}>
           <img src={product.imageUrls[0]} alt={product.imageUrls[0]} />
           {product.hit === 'yes' && (
