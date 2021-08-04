2aeecae0507109d67a087421471f31931e0f3b19
Vadim Budko <viaduksun@gmail.com>
2021-08-04 14:14:33 +0300

    fix

--- src/api/uploadImg.js
+++ src/api/uploadImg.js
@@ -2,20 +2,18 @@
 import axios from 'axios';
 
 const uploadImg = (imageSelected) => {
-  console.log('imageSelected===', imageSelected);
   const formData = new FormData();
   formData.append('file', imageSelected);
   formData.append('upload_preset', 'photo-store');
-  console.log('UPLOAD click');
-  console.log('FORM', formData);
-  const config = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      'Access-Control-Allow-Credentials': 'true',
-      'Content-Type': 'application/json',
-    }
+
+  const token = localStorage.getItem('token');
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    Authorization: token,
   };
+
   return axios
     .post(
       'https://api.cloudinary.com/v1_1/finalprojectfe242021/image/upload/', formData
--- src/components/ProfileBlock/EditProfileBigPreview.jsx
+++ src/components/ProfileBlock/EditProfileBigPreview.jsx
@@ -13,11 +13,14 @@ import styles from './ProfileBlock.module.scss';
 import Button from '../UI/Button/Button';
 import { updateCustomerAction } from '../../store/admin/actions';
 import uploadImg from '../../api/uploadImg';
+import Loader from '../UI/Loader/Loader';
 
 const EditProfile = () => {
   const dispatch = useDispatch();
   const [imageSelected, setImageSelected] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [previewSource, setPreviewSource] = useState('');
+  const [previewBigActive, setPreviewBigActive] = useState(false);
   const [cloudUrl, setCloudUrl] = useState('');
   const currentUser = useSelector((state) => state.admin.currentUser);
 
@@ -27,27 +30,47 @@ const EditProfile = () => {
     reader.readAsDataURL(file);
     reader.onloadend = () => {
       setPreviewSource(reader.result);
+      setPreviewBigActive(true);
     };
   };
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
+    // setImageSelected(file);
     previewFile(file);
   };
 
   const handleUploadImg = () => {
+    setIsLoading(true);
     if (!previewSource) return;
+    console.log('UPLOAD CLICK');
+    // console.log('previewSource', previewSource);
+    // uploadImg(imageSelected)
     uploadImg(previewSource)
       .then((result) => {
+        setPreviewBigActive(false);
         console.log('upload: ', result);
         setCloudUrl(result.data.url);
       })
+      .then(() => {
+        setIsLoading(false);
+      })
       .catch((err) => {
-        console.log(
-          err
-        ); /* Show error to customer, may be incorrect password or something else */
+        console.log(err);
       });
   };
-
+  // const handleUploadImg = () => {
+  //   if (!previewSource) return;
+  //   uploadImg(previewSource)
+  //     .then((result) => {
+  //       console.log('upload: ', result);
+  //       setCloudUrl(result.data.url);
+  //     })
+  //     .catch((err) => {
+  //       console.log(
+  //         err
+  //       ); /* Show error to customer, may be incorrect password or something else */
+  //     });
+  // };
   const handleSubmitForm = (values, { setSubmitting }) => {
     console.log('SUBMIT', cloudUrl);
     const { firstName, lastName, login, email, phone } = values;
@@ -67,13 +90,13 @@ const EditProfile = () => {
       console.log(form);
       dispatch(updateCustomerAction(form));
       setSubmitting(false);
+      setPreviewBigActive(false);
     }
   };
-  // const numericRegex = /(?=.*[0-9])/;
+
   const validate = Yup.object({
     firstName: Yup.string()
       .max(20, 'Must be 20 characters or less')
-      // .matches(numericRegex, 'Числовой символ недопустим')
       .required('Введите ваше имя'),
     lastName: Yup.string()
       .max(20, 'Must be 20 characters or less')
@@ -105,7 +128,7 @@ const EditProfile = () => {
         onSubmit={handleSubmitForm}
         validationSchema={validate}
       >
-        {(formik) => (
+        {({ isSubmitting }) => (
           <Form className={styles.formWrapper}>
             <div className={styles.inputsWrapper}>
               <TextInput label="Имя*" name="firstName" type="text" />
@@ -116,6 +139,13 @@ const EditProfile = () => {
               <div className={styles.avatarBlockWrapper}>
                 <p className={styles.avatarBlockTitle}>Аватар</p>
                 <div className={styles.avatarBlock}>
+                  {previewSource && previewBigActive && (
+                    <div className={styles.previewAvatarBigWrapper}>
+                      <div className={styles.previewAvatarBig}>
+                        <img src={previewSource} alt="avatar" />
+                      </div>
+                    </div>
+                  )}
                   <div className={styles.previewAvatar}>
                     {previewSource && <img src={previewSource} alt="avatar" />}
                     {!previewSource && currentUser.avatar && (
@@ -127,7 +157,6 @@ const EditProfile = () => {
                   </div>
                   <input
                     type="file"
-                    value={imageSelected}
                     onChange={handleFileInputChange}
                     className={styles.fileInput}
                     id="upload"
@@ -148,13 +177,27 @@ const EditProfile = () => {
               </div>
             </div>
             <div className="form-btn-group">
-              <Button type="submit" addClass="cart_green">
+              <Button
+                type="submit"
+                addClass="cart_green"
+                disabled={isSubmitting}
+              >
                 Готово
               </Button>
             </div>
+            {isSubmitting && (
+              <div className={styles.loaderWrapper}>
+                <Loader />
+              </div>
+            )}
           </Form>
         )}
       </Formik>
+      {isLoading && (
+        <div className={styles.loaderWrapper}>
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
