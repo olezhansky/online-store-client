2b96a43fe658f413579121508e710f179e3f5212
Oleg Ivanov <u014@Altera2.local>
2021-08-11 15:48:35 +0300

    added autocomplete

--- src/components/Header/SearchForm/SearchForm.jsx
+++ src/components/Header/SearchForm/SearchForm.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchIcon from '@material-ui/icons/Search';
 import { Link } from 'react-router-dom';
@@ -14,8 +17,22 @@ import styles from './SearchForm.module.scss';
 const SearchForm = () => {
   const dispatch = useDispatch();
   const searchValue = useSelector((state) => state.searchProducts.searchValue);
+  const [brands, setBrands] = useState([
+    'canon', 'nikon', 'sony', 'fuji'
+  ]);
+  const [state, setState] = useState([]);
+
   const handleChange = (e) => {
+    const newArr = brands.filter((item) => item.includes(searchValue));
+    setState(newArr);
     dispatch(setSearchValueAction(e.target.value));
+  };
+
+  const handleClick2 = (item) => {
+    dispatch(clearSearchProductsAction());
+    dispatch(setSearchValueForUSerAction(item));
+    dispatch(getSearchProductsAction(item));
+    dispatch(clearSearchValueAction());
   };
 
   const handleClick = () => {
@@ -24,7 +41,7 @@ const SearchForm = () => {
     dispatch(getSearchProductsAction(searchValue));
     dispatch(clearSearchValueAction());
   };
-  
+
   return (
     <div className={styles.searchFormWrapper}>
       <form className={styles.searchForm}>
@@ -35,6 +52,21 @@ const SearchForm = () => {
           placeholder="Search text"
           onChange={(e) => handleChange(e)}
         />
+        {searchValue && (
+        <ul className={styles.Autocomplete}>
+          {state.map((item) => (
+            <Link to="/search-products">
+              <li
+                className={styles.AutocompleteItem}
+                onClick={() => handleClick2(item)}
+              >
+                {item}
+              </li>
+            </Link>
+          ))}
+        </ul>
+)}
+     
         {!searchValue ? (
           <button
             className={styles.searchButton}
